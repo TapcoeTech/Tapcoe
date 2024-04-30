6a6c4181603cb92bcfbb49498c5d22c71e60be0c
Saurabh Mishra <saurabh.mishra@mastersunion.org>
2024-04-30 23:52:38 +0530

    added the share feedback button

--- tapcoe/src/components/Timer4.js
+++ tapcoe/src/components/Timer4.js
@@ -54,103 +54,104 @@ const Timer4 = ({startTime,eventName}) => {
         }
       }, [startTime]);
   return (
-    <div className="w-98 max-w-xl bg-white rounded-lg overflow-hidden shadow-lg">
-       <div className="flex items-center justify-center">
-       <img/>
-        <span className="text-2xl font-semibold" style={{color:"#004F4A"}}>{eventName}</span>
-       </div>
-      <div className=" p-4">
-        {/* Countdown Timer */}
-        <div className="flex items-center justify-center">
-          <div className="flex gap-3 sm:gap-1 flex-row bg-white h-36 rounded-lg overflow-hidden pt-2 pr-3 sm:pt-0 sm:pr-0">
-            <div className="flex flex-col bg-[#2A303C] sm:w-32 w-16">
-              <div className="h-10 sm:h-20 bg-[#2A303C]">
-                <div className="h-[30px] flex justify-center bg-[#2A303C] sm:text-3xl text-xl !text-[#A6ADBB]">
-                  <div
-                    className={
-                      countDownTime?.days >= 0 &&
-                      countDownTime?.hours == 23 &&
-                      countDownTime?.minutes == 59 &&
-                      countDownTime?.seconds == 59
-                        ? "animate-timer"
-                        : "relative top-5"
-                    }
-                  >
-                    {countDownTime?.days}
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <span className="text-lg sm:text-2xl text-center text-[#A6ADBB]">
-                  {countDownTime?.days == 1 ? "Day" : "Days"}
-                </span>
-              </div>
+<div className="w-full sm:w-64 bg-white overflow-hidden shadow-lg  hover:translate-x-3.5 ">
+    <div className="flex flex-row  items-center justify-center gap-2 ">
+        <div className="w-[40%]">
+<img src="event.png"/>
+        </div>
+        <div className="w-[60%]">
+            <p className="text-black-500 px-2 py-1 rounded-md text-xl font-semibold">The Friends</p>
             </div>
-            <div className="flex flex-col bg-[#2A303C] sm:w-32 w-16">
-              <div className="h-16 sm:h-20 bg-[#2A303C]">
-                <div className="h-[60px] flex justify-center bg-[#2A303C] sm:text-3xl text-xl !text-[#A6ADBB]">
-                  <div
-                    className={
-                      countDownTime?.hours >= 0 &&
-                      countDownTime?.minutes == 59 &&
-                      countDownTime?.seconds == 59
-                        ? "animate-timer"
-                        : "relative top-5"
-                    }
-                  >
-                    {countDownTime?.hours}
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <span className="text-lg sm:text-2xl text-center text-[#A6ADBB]">
-                  {countDownTime?.hours == 1 ? "Hour" : "Hours"}
-                </span>
-              </div>
-            </div>
-            <div className="flex flex-col bg-[#2A303C] sm:w-32 w-16">
-              <div className="h-16 sm:h-20 bg-[#2A303C]">
-                <div className="h-[60px] flex justify-center bg-[#2A303C] sm:text-3xl text-xl !text-[#A6ADBB]">
-                  <div
-                    className={
-                      countDownTime?.minutes >= 0 && countDownTime?.seconds == 59
-                        ? "animate-timer"
-                        : "relative top-5"
-                    }
-                  >
-                    {countDownTime?.minutes}
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <span className="text-lg sm:text-2xl text-center text-[#A6ADBB]">
-                  {countDownTime?.minutes == 1 ? "Minute" : "Minutes"}
-                </span>
-              </div>
-            </div>
-            <div className="flex flex-col bg-[#2A303C] sm:w-32 w-16">
-              <div className="h-16 sm:h-20 bg-[#2A303C]">
-                <div className="h-[60px] flex justify-center bg-[#2A303C] overflow-hidden sm:text-3xl text-xl text-[#A6ADBB]">
-                  <div ref={secondTimer}>{countDownTime?.seconds}</div>
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <span className="text-lg sm:text-2xl text-center text-[#A6ADBB]">
-                  {countDownTime?.seconds == 1 ? "Second" : "Seconds"}
-                </span>
+    </div>
+  <div className="p-2">
+    {/* Countdown Timer */}
+    <div className="flex items-center justify-center">
+      <div className="flex gap-3 flex-row h-14 overflow-hidden pt-2 pb-2 pl-2 pr-2 sm:pt-0 sm:pr-0">
+        <div className="flex flex-col w-1/4 sm:w-16 items-center">
+          <div className="h-10 w-6 bg-[#FACC15] rounded-md">
+            <div className="flex justify-center items-center h-full text-base text-[#000000]">
+              <div
+                className={
+                  countDownTime?.days >= 0 &&
+                  countDownTime?.hours == 23 &&
+                  countDownTime?.minutes == 59 &&
+                  countDownTime?.seconds == 59
+                    ? "animate-timer"
+                    : "relative top-1" 
+                }
+              >
+                {countDownTime?.days}
               </div>
             </div>
           </div>
+          <div className="mt-1">
+            <span className="text-xs text-[#A6ADBB]">Days</span>
+          </div>
         </div>
-
-        {/* Buttons */}
-        <div className="flex justify-center mt-4 space-x-4">
-          <button className="text-black-500 px-4 py-2 rounded-md text-xl font-semibold">Winners</button>
-          <button className="bg-yellow-400 px-4 py-2 rounded-md text-white  text-xl font-semibold">Open Event</button>
+        <div className="flex flex-col w-1/4 sm:w-16 items-center">
+          <div className="h-10 w-6 bg-[#FACC15] rounded-md">
+            <div className="flex justify-center items-center h-full text-base text-[#000000]">
+              <div
+                className={
+                  countDownTime?.hours >= 0 &&
+                  countDownTime?.minutes == 59 &&
+                  countDownTime?.seconds == 59
+                    ? "animate-timer"
+                    : "relative top-1"
+                }
+              >
+                {countDownTime?.hours}
+              </div>
+            </div>
+          </div>
+          <div className="mt-1">
+            <span className="text-xs text-[#A6ADBB]">Hours</span>
+          </div>
+        </div>
+        <div className="flex flex-col w-1/4 sm:w-16 items-center">
+          <div className="h-10  w-6 bg-[#FACC15] rounded-md">
+            <div className="flex justify-center items-center h-full bg-[#FACC15] text-base text-[#000000]">
+              <div
+                className={
+                  countDownTime?.minutes >= 0 && countDownTime?.seconds == 59
+                    ? "animate-timer"
+                    : "relative top-1"
+                }
+              >
+                {countDownTime?.minutes}
+              </div>
+            </div>
+          </div>
+          <div className="mt-1">
+            <span className="text-xs text-[#A6ADBB]">Minutes</span>
+          </div>
+        </div>
+        <div className="flex flex-col w-1/4 sm:w-16 items-center">
+          <div className="h-10 w-6 bg-[#FACC15] rounded-md">
+            <div className="flex justify-center items-center h-full text-base text-[#000000]">
+              <div ref={secondTimer}>{countDownTime?.seconds}</div>
+            </div>
+          </div>
+          <div className="mt-1">
+            <span className="text-xs text-[#A6ADBB]">Seconds</span>
+          </div>
         </div>
       </div>
-      
     </div>
+
+    {/* Buttons */}
+    <div className="flex justify-center mt-1 space-x-1">
+      <button className="text-black-500 px-2 py-1 rounded-md text-md font-semibold">Winners</button>
+      <button className="bg-yellow-400 px-2 py-1 rounded-md text-white text-md font-semibold">Open Event</button>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
   );
 };
 export default Timer4;
