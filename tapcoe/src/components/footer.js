10a3cc6e2c3fe842d3b9d44b4763c2836e56ee94
Saurabh Mishra <saurabh.mishra@mastersunion.org>
2024-05-13 21:20:57 +0530

    added the share feedback button

---
+++ tapcoe/src/components/footer.js
@@ -0,0 +1,8 @@
+export function Footer() {
+    return(
+        <div className="bg-gray-400">
+            
+
+        </div>
+    )
+}
--- tapcoe/src/pages/Home.js
+++ tapcoe/src/pages/Home.js
@@ -5,80 +5,99 @@ import Swipper from "../components/Swipper"
 import Timer4 from "../components/Timer4"
 
 const Home = () => {
-const [isMenu,setMenu]=useState(false);
+    const [isMenu, setMenu] = useState(false);
     const events = [
         {
-          startTime: "2024-06-10T02:30:00",
-          eventName: "Event 1",
-          eventAddress:"Kankarbgh,patna"
+            startTime: "2024-06-10T02:30:00",
+            eventName: "Event 1",
+            eventAddress: "Kankarbgh,patna"
         },
         {
-          startTime: "2024-06-11T08:00:00",
-          eventName: "Event 2",
-          eventAddress:"Delhi,karolBagh"
+            startTime: "2024-06-11T08:00:00",
+            eventName: "Event 2",
+            eventAddress: "Delhi,karolBagh"
         },
         {
-          startTime: "2024-06-12T14:45:00",
-          eventName: "Event 3",
-          eventAddress:"Gurgaon, sector 47"
+            startTime: "2024-06-12T14:45:00",
+            eventName: "Event 3",
+            eventAddress: "Gurgaon, sector 47"
         },
         {
-          startTime: "2024-06-13T18:20:00",
-          eventName: "Event 4",
-          eventAddress:"Chennai, santhom high road"
+            startTime: "2024-06-13T18:20:00",
+            eventName: "Event 4",
+            eventAddress: "Chennai, santhom high road"
         },
         {
-          startTime: "2024-06-14T21:10:00",
-          eventName: "Event 5",
-          eventAddress:"Chennai, santhom high road"
+            startTime: "2024-06-14T21:10:00",
+            eventName: "Event 5",
+            eventAddress: "Chennai, santhom high road"
         }
-      ];
-      const handleMenuState=()=>{
-setMenu(!isMenu);
+    ];
+    const handleMenuState = () => {
+        setMenu(!isMenu);
 
-      }
-      console.log(isMenu,"qqqqqqqqqqqqqq");
+    }
+   
     return (
- <div>
-           <div className='header'>
-            <Navbar  handleMenuState={handleMenuState}/>
-<div className={`${isMenu ? " opacity-25" :""}`}>
-<Swipper />
-            <div className="flex  flex-row justify-between items-center gap-8">
-  <div className="w-full w-[1/2] ml-4 md:ml-0 md:mr-2">
-    <img src="pic.png" className="w-full" alt="placeholder" />
-  </div>
-  <div className="flex flex-col items-center w-full md:w-[50%] justify-center md:mr-4">
-    <div className="flex flex-col gap-4 items-center text-center">
-      <span className="font-bold md:text-4xl text-lg">Experience Tapcoe</span>
-      <p className="w-full w-[1/2] md:text-2xl text-lg justify-center">
-        Are you a brand or an organization that wants to boost engagement in the
-        most creative way? Engage your users through our events in different categories.
-        Easily create interesting contests and events that would help your brand reach the masses
-        and create zeal and enthusiasm amongst them.
-      </p>
-    </div>
-  </div>
-</div>
-</div>
+        <div>
+            <div className='header'>
+                <Navbar handleMenuState={handleMenuState} />
+                <div className={`${isMenu ? " opacity-25" : ""}`}>
+                    <Swipper />
+                    {/* <div className="flex  flex-row justify-between items-center gap-8">
+                        <div className="w-full w-[1/2] ml-4 md:ml-0 md:mr-2">
+                            <img src="pic.png" className="w-full" alt="placeholder" />
+                        </div>
+                        <div className="flex flex-col items-center w-full md:w-[50%] justify-center md:mr-4">
+                            <div className="flex flex-col gap-4 items-center text-center">
+                                <span className="font-bold md:text-4xl text-lg">Experience Tapcoe</span>
+                                <p className="w-full w-[1/2] md:text-2xl text-lg justify-center">
+                                    Are you a brand or an organization that wants to boost engagement in the
+                                    most creative way? Engage your users through our events in different categories.
+                                    Easily create interesting contests and events that would help your brand reach the masses
+                                    and create zeal and enthusiasm amongst them.
+                                </p>
+                            </div>
+                        </div>
+                    </div> */}
 
+
+                </div>
+
+                <div className="p-4 md:flex grid justify-center items-center gap-10 mt-[100px]">
+                    <div>
+                        {/* <img src="pic.png" className="w-[60%]" alt="placeholder" /> */}
+                        <img src="images/experience.png" />
+                    </div>
+                    <div className="md:-mt-[100px]">
+                        <h1 className="font-semibold text-[30px] mt-6 md:grid flex justify-center">Experience Tapcoe</h1>
+                        <p className="md:w-[600px] md:grid flex justify-center">
+                            Are you a brand or an organization that wants to boost the
+                            engagement in the most creative way. Engage your users
+                            through our events in different category. Easily create 
+                            interesting contest and events that would help your brand 
+                            to reach the masses and to create zeal and enthusiasm 
+                            amongst them.
+                        </p>
+
+
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div className="flex flex-wrap justify-center mt-4">
+                {events?.map((value, index) => {
+                    return (
+                        <div className="flex mx-2 mb-2 " key={index}>
+                            <Timer4 startTime={value?.startTime} eventName={value?.eventName} eventAddress={value?.eventAddress} />
+                        </div>
+                    );
+                })}
+            </div>
+            
         </div>
-        <div className="flex flex-wrap justify-center mt-4">
-  {events?.map((value, index) => {
-    return (
-      <div className="flex mx-2 mb-2 "  key={index}>
-        <Timer4 startTime={value?.startTime} eventName={value?.eventName} eventAddress={value?.eventAddress} />
-      </div>
-    );
-  })}
-</div>
-<div className="flex items-center justify-center text-center font-bold text-black-500 text-xl mt-8"><span>Story So Far</span></div>
-<div className="flex flex-row justify-center items-center text-center ">
-<div className="border-orange-600 border-8 bg-red w-[30%]">1dfghjkdfghjk</div>
-<div className="w-[30%]">2</div>
-<div className="w-[30%]">3</div>
-</div>
- </div>
     )
 }
 
