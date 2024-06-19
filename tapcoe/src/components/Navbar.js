523323fa1c225183bc3d697c5b6feffebf598dff
Saurabh Mishra <saurabh.mishra@mastersunion.org>
2024-06-19 12:25:38 +0530

    added the navigation

--- tapcoe/src/components/Navbar.js
+++ tapcoe/src/components/Navbar.js
@@ -36,7 +36,7 @@ function Navbar({ handleMenuState }) {
             setName(storedName);
             setEmail(storedEmail);
 
-          
+         
         } else {
             setIsLoggedIn(false);
         }
