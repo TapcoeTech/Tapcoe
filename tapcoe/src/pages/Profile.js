62645a376f97ed978f2fae009b2a0e35e895f50e
saurabh1352 <mishrasau91@gmail.com>
2024-09-23 16:51:07 +0530

    api changes

--- tapcoe/src/pages/Profile.js
+++ tapcoe/src/pages/Profile.js
@@ -5,6 +5,7 @@ import { toast } from "react-toastify";
 import Cookies from "js-cookie";
 import { useLocation, useNavigate } from 'react-router-dom';
 import ShareButtons from "../components/ShareButtons";
+import Loader from "../components/Loader";
 export const Profile = () => {
     const [participant, setParticipant] = useState(null);
     const [openShare, setOpenShare] = useState(null); // Track open share for a specific participant
@@ -130,97 +131,99 @@ const navigate = useNavigate();
         setShareUrl(shareUrl);  // Save the generated share URL to be used in ShareButtons
     };
     return (
-        <div class="flex flex-col-reverse md:flex-row justify-center items-center md:gap-8 gap-4 p-4 mt-10">
+   <>
+   {loading ? <Loader/> :     <div class="flex flex-col-reverse md:flex-row justify-center items-center md:gap-8 gap-4 p-4 mt-10">
 
 
-        <div class="max-w-sm bg-white rounded-3xl shadow dark:bg-gray-800 min-w-[400px] sm:min-w-auto flex flex-col mb-4">
-            <div class="max-w-full">
-                <a href="#">
-                    <img class="rounded-t-lg min-w-[400px] min-h-[400px] w-full h-auto object-cover" src={participant?.participant?.image?.imageUrl} alt="Event" />
-                </a>
-            </div>
-    
-            <div class="flex flex-row p-4 justify-center items-center">
-                <div class="flex p-3 items-center gap-2 border-gray-400">
-                    <img src="/images/pointer.png" alt="Pointer" />
-                    <p class="text-[12px] font-montserrat text-[#004F4A]">Tap ID: 3456</p>
-                </div>
-                <div class="border-l border-gray-400 dark:border-gray-700 h-12 self-stretch mx-4"></div>
-                <div class="flex flex-row p-3 items-center gap-2">
-                    <img src="/images/ranking.png" class="w-6" alt="Ranking" />
-                    <p class="text-[12px] font-montserrat text-[#004F4A]">Rank: {participant?.participant?.rank}</p>
-                </div>
-            </div>
-    
-            <div class="border-t border-gray-400 dark:border-gray-700 mx-4"></div>
-    
-            <div class="flex gap-4 justify-center items-center mt-2 py-2">
-                
-                <img src="./images/heart.png" alt="Heart" />
-                <div>Total Tap: {participant?.likesLength}</div>
-            </div>
-    
-            <div class="flex flex-col gap-4 justify-center items-center pb-4">
-                <div class="text-center p-4">
-                    <p style={{color:"#666"}}>
-                        Lorem is a simple to showcase the text of the printing and typesetting industry.
-                    </p>
-                </div>
-                <div class="flex flex-row gap-4 items-center justify-center">
-                    <div>
-                        <p><strong>Uploaded</strong> By: {participant?.user?.name}</p>
-                    </div>
-                    <div>
-                        <p><strong>Uploaded Time</strong>: 11:50:36 20/04/24</p>
-                    </div>
-                </div>
-                <div class="flex gap-4">
-                    <button class="flex flex-row items-center bg-yellow-500 rounded-md px-4 py-2 hover:bg-yellow-600 active:bg-yellow-700 transition duration-200" aria-label="Tap" onClick={() => {handleSubmit()}}>
-                      
+<div class="max-w-sm bg-white rounded-3xl shadow dark:bg-gray-800 min-w-[400px] sm:min-w-auto flex flex-col mb-4">
+    <div class="max-w-full">
+        <a href="#">
+            <img class="rounded-t-lg min-w-[400px] min-h-[400px] w-full h-auto object-cover" src={participant?.participant?.image?.imageUrl} alt="Event" />
+        </a>
+    </div>
 
-                        {participant?.participant?.likes.includes(localStorage.getItem('_id')) ?    <img src="./images/save.png" alt="Heart icon" class="mr-2 h-5 w-5" />:  <img src="./images/heart.png" alt="Heart icon" class="mr-2" /> }
-                        <div>Tap</div>
-                    </button>
-                    <button class="flex flex-row items-center bg-yellow-500 rounded-md px-4 py-2 hover:bg-yellow-600 active:bg-yellow-700 transition duration-200" aria-label="Share"
-                    
-                    onClick={() => handleShareClick()}
-                    >
-                        <img src="./images/share.png" alt="Share icon" class="mr-2" />
-                        <div>Share</div>
-                    </button>
-                </div>
-                {openShare === localStorage.getItem('paticipant_id') && shareUrl && (
-                                    <div className="flex justify-center mt-4">
-                                        <ShareButtons shareUrl={shareUrl} />
-                                    </div>
-                                )}
+    <div class="flex flex-row p-4 justify-center items-center">
+        <div class="flex p-3 items-center gap-2 border-gray-400">
+            <img src="/images/pointer.png" alt="Pointer" />
+            <p class="text-[12px] font-montserrat text-[#004F4A]">Tap ID: 3456</p>
+        </div>
+        <div class="border-l border-gray-400 dark:border-gray-700 h-12 self-stretch mx-4"></div>
+        <div class="flex flex-row p-3 items-center gap-2">
+            <img src="/images/ranking.png" class="w-6" alt="Ranking" />
+            <p class="text-[12px] font-montserrat text-[#004F4A]">Rank: {participant?.participant?.rank}</p>
+        </div>
+    </div>
+
+    <div class="border-t border-gray-400 dark:border-gray-700 mx-4"></div>
+
+    <div class="flex gap-4 justify-center items-center mt-2 py-2">
+        
+        <img src="./images/heart.png" alt="Heart" />
+        <div>Total Tap: {participant?.likesLength}</div>
+    </div>
+
+    <div class="flex flex-col gap-4 justify-center items-center pb-4">
+        <div class="text-center p-4">
+            <p style={{color:"#666"}}>
+                Lorem is a simple to showcase the text of the printing and typesetting industry.
+            </p>
+        </div>
+        <div class="flex flex-row gap-4 items-center justify-center">
+            <div>
+                <p><strong>Uploaded</strong> By: {participant?.user?.name}</p>
+            </div>
+            <div>
+                <p><strong>Uploaded Time</strong>: 11:50:36 20/04/24</p>
             </div>
         </div>
-    
-      
-        <div class="flex flex-col md:flex-row gap-4 justify-center items-center md:pl-4 bg-white border border-gray-500 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
-            <div class="">
-                <img class="object-cover" src={participant?.eventImage} alt="Icon" />
+        <div class="flex gap-4">
+            <button class="flex flex-row items-center bg-yellow-500 rounded-md px-4 py-2 hover:bg-yellow-600 active:bg-yellow-700 transition duration-200" aria-label="Tap" onClick={() => {handleSubmit()}}>
+              
+
+                {participant?.participant?.likes.includes(localStorage.getItem('_id')) ?    <img src="./images/save.png" alt="Heart icon" class="mr-2 h-5 w-5" />:  <img src="./images/heart.png" alt="Heart icon" class="mr-2" /> }
+                <div>Tap</div>
+            </button>
+            <button class="flex flex-row items-center bg-yellow-500 rounded-md px-4 py-2 hover:bg-yellow-600 active:bg-yellow-700 transition duration-200" aria-label="Share"
+            
+            onClick={() => handleShareClick()}
+            >
+                <img src="./images/share.png" alt="Share icon" class="mr-2" />
+                <div>Share</div>
+            </button>
+        </div>
+        {openShare === localStorage.getItem('paticipant_id') && shareUrl && (
+                            <div className="flex justify-center mt-4">
+                                <ShareButtons shareUrl={shareUrl} />
+                            </div>
+                        )}
+    </div>
+</div>
+
+
+<div class="flex flex-col md:flex-row gap-4 justify-center items-center md:pl-4 bg-white border border-gray-500 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
+    <div class="">
+        <img class="object-cover" src={participant?.eventImage} alt="Icon" />
+    </div>
+    <div class="flex flex-col sm:flex-row items-center sm:items-start gap-2 sm:gap-4">
+        <div class="flex flex-col items-center gap-2 sm:gap-4">
+            <div class="text-4xl font-semibold font-montserrat text-green-800 whitespace-nowrap">
+              {participant?.eventName}
             </div>
-            <div class="flex flex-col sm:flex-row items-center sm:items-start gap-2 sm:gap-4">
-                <div class="flex flex-col items-center gap-2 sm:gap-4">
-                    <div class="text-4xl font-semibold font-montserrat text-green-800 whitespace-nowrap">
-                      {participant?.eventName}
-                    </div>
-                    <span className="font-normal font-montserrat text-green-800 text-base">
-    {/* Render HTML content */}
-    <span dangerouslySetInnerHTML={{ __html: participant?.description }} />
-  
+            <span className="font-normal font-montserrat text-green-800 text-base">
+{/* Render HTML content */}
+<span dangerouslySetInnerHTML={{ __html: participant?.description }} />
+
 </span>
 
-                </div>
-            </div>
-            <div class="px-6 py-4">
-                <Countdown startTime={participant?.endDate} />
-            </div>
         </div>
-    
     </div>
+    <div class="px-6 py-4">
+        <Countdown startTime={participant?.endDate} />
+    </div>
+</div>
+
+</div>}
+   </>
     
 
     )
